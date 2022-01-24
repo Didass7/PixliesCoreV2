de6d83f9d92be06595e286687e98062db3846212
MickMMars <a.khamisipour@outlook.de>
2022-01-24 15:41:20 +0100

    + Started module command

--- PixliesCore/src/main/java/net/pixlies/core/commands/debug/ModulesCommand.java
+++ PixliesCore/src/main/java/net/pixlies/core/commands/debug/ModulesCommand.java
@@ -3,10 +3,16 @@ package net.pixlies.core.commands.debug;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.*;
+import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
+import com.github.stefvanschie.inventoryframework.pane.PaginatedPane;
+import com.github.stefvanschie.inventoryframework.pane.Pane;
+import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import net.pixlies.core.Main;
 import net.pixlies.core.modules.ModuleDescription;
 import net.pixlies.core.modules.Module;
+import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,6 +27,17 @@ public class ModulesCommand extends BaseCommand {
     @Description("Returns all loaded Modules")
     public void onModules(CommandSender commandSender) {
         commandSender.sendMessage("PixliesCoreV2 Modules: " + getModuleNames());
+
+        ChestGui gui = new ChestGui(3, "§aModules");
+        PaginatedPane modulesPane = new PaginatedPane(0, 0, 9, 5);
+
+        modulesPane.populateWithGuiItems(new ArrayList<>(){{
+            for(Map.Entry<Module, ModuleDescription> entry : modules.entrySet()) {
+                Material icon = entry.getValue().getIcon();
+
+            }
+        }});
+
     }
 
     @HelpCommand
@@ -28,11 +45,9 @@ public class ModulesCommand extends BaseCommand {
         help.showHelp();
     }
 
-    public ModulesCommand() {
+    public ModulesCommand() { }
 
-    }
-
-    private Map<Module, ModuleDescription> modules = Main.getInstance().getModuleManager().getModules();
+    private final Map<Module, ModuleDescription> modules = Main.getInstance().getModuleManager().getModules();
 
     private List<String> getModuleNames() {
         List<String> moduleNames = new ArrayList<>();
