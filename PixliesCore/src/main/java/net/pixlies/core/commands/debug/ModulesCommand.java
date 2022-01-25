6adeb22203c1c3c807050e4516f41fc0e6f37817
MickMMars <a.khamisipour@outlook.de>
2022-01-25 23:40:54 +0100

    + Modules command has a GUI now

--- PixliesCore/src/main/java/net/pixlies/core/commands/debug/ModulesCommand.java
+++ PixliesCore/src/main/java/net/pixlies/core/commands/debug/ModulesCommand.java
@@ -3,6 +3,7 @@ package net.pixlies.core.commands.debug;
 import co.aikar.commands.BaseCommand;
 import co.aikar.commands.CommandHelp;
 import co.aikar.commands.annotation.*;
+import com.github.stefvanschie.inventoryframework.gui.GuiItem;
 import com.github.stefvanschie.inventoryframework.gui.type.ChestGui;
 import com.github.stefvanschie.inventoryframework.pane.PaginatedPane;
 import com.github.stefvanschie.inventoryframework.pane.Pane;
@@ -10,13 +11,17 @@ import com.github.stefvanschie.inventoryframework.pane.StaticPane;
 import net.pixlies.core.Main;
 import net.pixlies.core.modules.ModuleDescription;
 import net.pixlies.core.modules.Module;
+import net.pixlies.core.utils.ItemBuilder;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.StringJoiner;
 
 @CommandAlias("modules")
 @CommandPermission("pixlies.debug.modules")
@@ -26,7 +31,7 @@ public class ModulesCommand extends BaseCommand {
     @CommandCompletion("")
     @Description("Returns all loaded Modules")
     public void onModules(CommandSender commandSender) {
-        commandSender.sendMessage("PixliesCoreV2 Modules: " + getModuleNames());
+        // commandSender.sendMessage("PixliesCoreV2 Modules: " + getModuleNames());
 
         ChestGui gui = new ChestGui(3, "§aModules");
         PaginatedPane modulesPane = new PaginatedPane(0, 0, 9, 5);
@@ -34,10 +39,21 @@ public class ModulesCommand extends BaseCommand {
         modulesPane.populateWithGuiItems(new ArrayList<>(){{
             for(Map.Entry<Module, ModuleDescription> entry : modules.entrySet()) {
                 Material icon = entry.getValue().getIcon();
+                StringJoiner authorJoiner = new StringJoiner("§7, ");
+                for (String author : entry.getValue().getAuthors())
+                    authorJoiner.add("§b" + author);
+                ChatColor nameColor = entry.getValue().isActivated() ? ChatColor.GREEN : ChatColor.RED;
+                ItemBuilder builder = new ItemBuilder(icon)
+                        .setDisplayName(nameColor + entry.getValue().getName() + " v" + entry.getValue().getVersion())
+                        .addLoreLine("§7by " + authorJoiner.toString());
 
+                add(new GuiItem(builder.build(), e -> e.setCancelled(true)));
             }
         }});
 
+        gui.addPane(modulesPane);
+
+        gui.show((Player) commandSender);
     }
 
     @HelpCommand
