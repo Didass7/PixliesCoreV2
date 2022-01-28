092261082d3b3b2a02637e2d90748b51be777375
vPrototype <65795488+vyketype@users.noreply.github.com>
2022-01-27 21:55:53 -0500

    PixliesFun module

---
+++ PixliesFun/src/main/java/net/pixlies/pixliesfun/PixliesFun.java
@@ -0,0 +1,17 @@
+package net.pixlies.pixliesfun;
+
+import net.pixlies.core.modules.Module;
+
+public class PixliesFun extends Module {
+
+    @Override
+    public void onLoad() {
+        // TODO
+    }
+
+    @Override
+    public void onDrop() {
+        // TODO
+    }
+
+}
