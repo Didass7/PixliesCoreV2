c9ca5fc38469df258abe6599ef0d05e32dd8334a
dynmie <xbottam@gmail.com>
2022-01-27 20:35:45 -0800

    joinlistener added some things

--- LobbyModule/src/main/java/net/pixlies/lobby/listeners/impl/JoinListener.java
+++ LobbyModule/src/main/java/net/pixlies/lobby/listeners/impl/JoinListener.java
@@ -9,18 +9,21 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.Objects;
 
 public class JoinListener implements Listener {
 
-    private static final Lobby instance = Lobby.getInstance();
     private final Config config = Lobby.getInstance().getConfig();
 
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
 
         Player player = event.getPlayer();
+        player.getActivePotionEffects().forEach(effect -> player.removePotionEffect(effect.getType()));
+        player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, config.getInt("walkspeed.level", 1), false, false, false));
         player.setBedSpawnLocation(null, true);
         player.setExp(0);
         player.setHealth(Objects.requireNonNull(player.getAttribute(Attribute.GENERIC_MAX_HEALTH)).getBaseValue());
