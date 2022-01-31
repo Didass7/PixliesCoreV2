70a30fad64a6840cf6dd8c66c3e60831f8fe9399
dynmie <xbottam@gmail.com>
2022-01-30 16:35:54 -0800

    possible fix chat messages

--- PixliesCore/src/main/java/net/pixlies/core/listeners/cosmetics/ChatEmojiListener.java
+++ PixliesCore/src/main/java/net/pixlies/core/listeners/cosmetics/ChatEmojiListener.java
@@ -2,6 +2,7 @@ package net.pixlies.core.listeners.cosmetics;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
 import net.pixlies.core.utils.Emojis;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -11,9 +12,8 @@ public class ChatEmojiListener implements Listener {
 
     @EventHandler(priority = EventPriority.MONITOR)
     public void onChat(AsyncChatEvent event) {
-        if (!event.isCancelled()) {
-            event.message(Component.text(Emojis.replaceEmojis(event.message().toString())));
-        }
+        String message = LegacyComponentSerializer.legacyAmpersand().serialize(event.message());
+        event.message(Component.text(Emojis.replaceEmojis(message)));
     }
 
 }
