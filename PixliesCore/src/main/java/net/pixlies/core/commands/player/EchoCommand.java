b4d98279cd401f716c59a5d3afb4f2c929a0d73a
dynmie <xbottam@gmail.com>
2022-02-01 16:58:56 -0800

    echo command to test messages

---
+++ PixliesCore/src/main/java/net/pixlies/core/commands/player/EchoCommand.java
@@ -0,0 +1,18 @@
+package net.pixlies.core.commands.player;
+
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Default;
+import net.pixlies.core.utils.CC;
+import org.bukkit.command.CommandSender;
+
+@CommandPermission("pixlies.player.echo")
+public class EchoCommand {
+
+    @Default
+    @CommandAlias("echo")
+    public void onEcho(CommandSender sender, String text) {
+        sender.sendMessage(CC.format(text));
+    }
+
+}
