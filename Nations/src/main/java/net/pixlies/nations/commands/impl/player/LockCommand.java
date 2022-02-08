7ccc7c4bc32d2d4f9f419b39c811fe612e4205a2
dynmie <xbottam@gmail.com>
2022-02-07 20:44:30 -0800

    minor changes to locked chests

--- Nations/src/main/java/net/pixlies/nations/commands/impl/player/LockCommand.java
+++ Nations/src/main/java/net/pixlies/nations/commands/impl/player/LockCommand.java
@@ -21,44 +21,44 @@ public class LockCommand extends BaseCommand {
 
     private static final Main instance = Main.getInstance();
 
-    @Default
+    @Default // TODO: Staff bypass
     @Description("Locks/unlocks a chest")
     public void onLock(Player player) {
+
         User user = User.get(player.getUniqueId());
 
         Block block = player.getTargetBlock(5);
-        if (block == null || !block.getType().equals(Material.CHEST)) {
+        if (block == null || block.getType() != Material.CHEST || !(block.getState() instanceof TileState state)) {
             Lang.MUST_BE_A_PLAYER.send(player);
             return;
         }
 
-        TileState state = (TileState) block.getState();
         PersistentDataContainer container = state.getPersistentDataContainer();
-        NamespacedKey key = new NamespacedKey(instance, "locked-chests");
+        NamespacedKey key = new NamespacedKey(instance, "nations:locked-chests");
         String ownerUuid = container.getOrDefault(key, PersistentDataType.STRING, "none");
 
-        // If chest already is locked
-        if (container.has(key, PersistentDataType.STRING)) {
-            // If owner is not the player AND if player is not in staff mode
-            if (!ownerUuid.equals(player.getUniqueId().toString()) /* TODO: nation bypass is enabled */) {
-                Lang.CHEST_BELONGS_TO_OTHER.send(player, "%PLAYER%;" + ownerUuid);
-            } else {
-                container.set(key, PersistentDataType.STRING, "none");
-                Lang.CHEST_UNLOCKED.send(player);
-                player.playSound(player.getLocation(), "entity.arrow.hit_player", 100, 1);
+        if (container.has(key, PersistentDataType.PrimitivePersistentDataType.STRING)) {
+
+            if (!ownerUuid.equals(player.getUniqueId().toString())) {
+                Lang.CHEST_BELONGS_TO_OTHER.send(player, "%PLAYER%;" + user.getNickName());
+                return;
             }
-        } else {
-            container.set(key, PersistentDataType.STRING, player.getUniqueId().toString());
-            Lang.CHEST_LOCKED.send(player);
-            player.playSound(player.getLocation(), "block.chest.locked", 100, 1);
+
+            container.remove(key);
+            player.playSound(player.getLocation(), "entity.arrow.hit_player", Float.MAX_VALUE, 1);
+            Lang.CHEST_UNLOCKED.send(player);
+
+            state.update();
+            return;
+
         }
 
-        state.update();
-    }
+        container.set(key, PersistentDataType.PrimitivePersistentDataType.STRING, player.getUniqueId().toString());
+        Lang.CHEST_LOCKED.send(player);
+        player.playSound(player.getLocation(), "block.chest.locked", Float.MAX_VALUE, 1);
 
-    @HelpCommand
-    public void onHelp(CommandSender sender, CommandHelp help) {
-        help.showHelp();
+        state.update();
+
     }
 
 }
--- Nations/src/main/java/net/pixlies/nations/events/listeners/LockedChestListener.java
+++ Nations/src/main/java/net/pixlies/nations/events/listeners/LockedChestListener.java
@@ -19,27 +19,32 @@ public class LockedChestListener implements Listener {
 
     private static final Main instance = Main.getInstance();
 
-    @EventHandler
+    @EventHandler // TODO: Nation bypass
     public void onInteract(PlayerInteractEvent event) {
+
         Player player = event.getPlayer();
         Block block = event.getClickedBlock();
-        if (block != null && block.getType() == Material.CHEST) {
-            if ((event.getAction() == Action.RIGHT_CLICK_BLOCK || event.getAction() == Action.LEFT_CLICK_BLOCK)) {
-                User user = User.get(player.getUniqueId());
 
-                TileState state = (TileState) block.getState();
-                PersistentDataContainer container = state.getPersistentDataContainer();
-                NamespacedKey key = new NamespacedKey(instance, "locked-chests");
-                String ownerUuid = container.getOrDefault(key, PersistentDataType.STRING, "none");
+        if (block == null || block.getType() != Material.CHEST) return;
+        if (!(block instanceof TileState state)) return;
 
-                // TODO: Nation bypass
-                if (container.has(key, PersistentDataType.STRING) && ownerUuid.equals(player.getUniqueId().toString()) /* && !user.isStaffModeEnabled() */) {
-                    event.setCancelled(true);
-                    Lang.CHEST_BELONGS_TO_OTHER.send(player);
-                    player.playSound(player.getLocation(), "block.chest.locked", 100, 1);
-                }
-            }
-        }
+        if (event.getAction() != Action.RIGHT_CLICK_BLOCK || event.getAction() != Action.LEFT_CLICK_BLOCK)
+            return;
+
+        NamespacedKey key = new NamespacedKey(instance, "nations:locked-chests");
+        PersistentDataContainer container = state.getPersistentDataContainer();
+
+        if (!container.has(key, PersistentDataType.PrimitivePersistentDataType.STRING)) return;
+
+        String ownerUuid = container.get(key, PersistentDataType.PrimitivePersistentDataType.STRING);
+        if (ownerUuid == null) return;
+
+        if (ownerUuid.equals(player.getUniqueId().toString())) return;
+
+        player.playSound(player.getLocation(), "block.chest.locked", Float.MAX_VALUE, 1);
+        Lang.CHEST_BELONGS_TO_OTHER.send(player);
+        event.setCancelled(true);
+
     }
 
 }
