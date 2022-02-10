35226b5c7da25c8370e3ecc5ca125040e0d19b38
dynmie <xbottam@gmail.com>
2022-02-09 16:58:50 -0800

    fix some proxy lang

--- PixliesProxyCore/src/main/java/net/pixlies/proxy/localization/Lang.java
+++ PixliesProxyCore/src/main/java/net/pixlies/proxy/localization/Lang.java
@@ -2,13 +2,20 @@ package net.pixlies.proxy.localization;
 
 import lombok.Getter;
 import net.md_5.bungee.api.CommandSender;
+import net.md_5.bungee.api.chat.TextComponent;
 import net.md_5.bungee.api.connection.ProxiedPlayer;
 import net.pixlies.proxy.Proxy;
 import net.pixlies.proxy.utils.CC;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
+/**
+ * Lang for proxy
+ * @author MickMMars
+ * @author Dynmie
+ */
 public enum Lang {
 
     TEST("", new HashMap<>()); // TODO
@@ -25,85 +32,69 @@ public enum Lang {
         this.languages = languages;
     }
 
-    public String get(CommandSender sender) {
-//        if (languages == null) return "";
-//        try {
-//            if (sender instanceof Player player) {
-//                User user = User.get(player.getUniqueId());
-//                String lang = user.getLang();
-//                if (languages.containsKey(lang)) {
-//                    return PREFIX + CC.format(languages.get(user.getLang()));
-//                } else {
-//                    return PREFIX + CC.format(languages.get("ENG"));
-//                }
-//            }
-//        } catch (Exception ignored) {
-//
-//        }
-        return PREFIX + CC.format(languages.get("ENG"));
+    public String get(CommandSender sender) { // TODO: User specified localization.
+        if (languages == null) return "";
+        if (languages.isEmpty()) return "";
+        if (sender instanceof ProxiedPlayer player) {
+            Locale locale = player.getLocale();
+            String name = locale.getISO3Language();
+            if (languages.containsKey(name)) {
+                return PREFIX + getRaw(name);
+            }
+        }
+        return get();
+    }
+
+    public String get(String lang) {
+        return PREFIX + getRaw(lang);
     }
 
     public String getRaw(String language) {
-        return languages.get(language).replace("&", "§");
+        if (languages.get(language) == null) return get();
+        return CC.format(languages.get(language));
+    }
+
+    /**
+     * Shortened get.
+     * @author Dynmie
+     * @return message with default ENG locale.
+     */
+    public String get() {
+        if (languages == null) return "";
+        if (languages.isEmpty()) return "";
+        return PREFIX + getRaw("ENG");
     }
 
     public boolean send(CommandSender sender) {
-        if (sender == null)
-            return false;
-        sender.sendMessage(get(sender));
+        if (sender == null) return false;
+        sender.sendMessage(new TextComponent(get(sender)));
         return true;
     }
 
     public boolean sendWithLangName(CommandSender sender, String langName) {
-        sender.sendMessage(PREFIX + languages.get(langName).replace("&", "§"));
+        sender.sendMessage(new TextComponent(get(langName)));
         return true;
     }
 
-    public void broadcast(Map<String, String> placeholders) {
-        for (ProxiedPlayer player : instance.getProxy().getPlayers()) {
-            String s = get(player);
-            for (Map.Entry<String, String> entry : placeholders.entrySet()) {
-                s = s.replace(entry.getKey(), entry.getValue());
-            }
-            player.sendMessage(s);
-        }
-    }
-
-    public void broadcast(Map<String, String> placeholders, String permission) {
-        for (ProxiedPlayer player : instance.getProxy().getPlayers()) {
-            if (!player.hasPermission(permission)) continue;
-            String s = get(player);
-            for (Map.Entry<String, String> entry : placeholders.entrySet()) {
-                s = s.replace(entry.getKey(), entry.getValue());
-            }
-            player.sendMessage(s);
-        }
-    }
-
     public void broadcast(String... placeholders) {
-        for (ProxiedPlayer player : instance.getProxy().getPlayers())
-            send(player, placeholders);
+        String message = replacePlaceholders(placeholders);
+        instance.getProxy().broadcast(new TextComponent());
     }
 
     public void broadcastPermission(String permission, String... placeholders) {
         for (ProxiedPlayer player : instance.getProxy().getPlayers())
             if (player.hasPermission(permission))
                 send(player, placeholders);
+        instance.getProxy().getConsole().sendMessage(new TextComponent(get()));
     }
 
-    public void kickPlayer(CommandSender sender) {
-        ProxiedPlayer player = (ProxiedPlayer) sender;
-        player.disconnect(get(sender));
+    public void kickPlayer(ProxiedPlayer player) {
+        player.disconnect(new TextComponent(get(player)));
     }
 
-    public void kickPlayer(CommandSender sender, String... placeholders) {
-        ProxiedPlayer player = (ProxiedPlayer) sender;
-        String send = get(sender);
-        for (String s : placeholders) {
-            String[] pSplit = s.split(";");
-            send = send.replace(pSplit[0], pSplit[1]);
-        }
-        player.disconnect(send);
+    public void kickPlayer(ProxiedPlayer player, String... placeholders) {
+        String send = replacePlaceholders(player, placeholders);
+        player.disconnect(new TextComponent(send));
     }
 
     public void setLanguage(Map<String, String> languages) {
@@ -118,13 +109,37 @@ public enum Lang {
      */
     public boolean send(CommandSender sender, String... placeholders) {
         if (sender == null) return false;
-        String send = get(sender);
-        for (String s : placeholders) {
-            String[] pSplit = s.split(";");
-            send = send.replace(pSplit[0], pSplit[1]);
-        }
-        sender.sendMessage(send);
+        String send = replacePlaceholders(sender, placeholders);
+        sender.sendMessage(new TextComponent(send));
         return true;
+    }
+
+    /**
+     * Replace placeholders.
+     * @author Dynmie
+     * @param sender the sender
+     * @param placeholders the placeholders, with ";" split
+     * @return message with placeholders replaced
+     */
+    private String replacePlaceholders(CommandSender sender, String... placeholders) {
+        String toReturn = sender == null ? get() : get(sender);
+        for (String string : placeholders) {
+            String[] split = string.split(";");
+            String placeholder = split[0] == null ? "" : split[0];
+            String replacement = split[1] == null ? "" : split[1];
+            toReturn = toReturn.replace(placeholder, replacement);
+        }
+        return toReturn;
+    }
+
+    /**
+     * Replace placeholders, shortened way.
+     * @author Dynmie
+     * @param placeholders the placeholders, with ";" split
+     * @return message with placeholders replaced
+     */
+    private String replacePlaceholders(String... placeholders) {
+        return replacePlaceholders(null, placeholders);
     }
 
 //    @SuppressWarnings("ResultOfMethodCallIgnored") // folder.mkdirs() is safe to ignore
