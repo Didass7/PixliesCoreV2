d026f2ca0b4f4618b15674427e63cdd6090de0a6
dynmie <xbottam@gmail.com>
2022-02-11 19:02:59 -0800

    some changes to ban listener

--- PixliesCore/src/main/java/net/pixlies/core/listeners/moderation/BanListener.java
+++ PixliesCore/src/main/java/net/pixlies/core/listeners/moderation/BanListener.java
@@ -4,8 +4,6 @@ import net.kyori.adventure.text.Component;
 import net.pixlies.core.entity.User;
 import net.pixlies.core.localization.Lang;
 import net.pixlies.core.moderation.Punishment;
-import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
@@ -15,31 +13,26 @@ import java.util.Date;
 
 public class BanListener implements Listener {
 
-    private static final String BAN_BROADCAST_PERMISSION = "earth.ban.broadcast";
-
     @EventHandler
     public void onLogin(PlayerLoginEvent event) {
         User user = User.get(event.getPlayer().getUniqueId());
-        if (user.getCurrentPunishments().containsKey("ban")) {
-            Punishment punishment = user.getBan();
-            String banMessage = Lang.BAN_MESSAGE.get(event.getPlayer())
-                            .replace("%REASON%", punishment.getReason())
-                            .replace("%BAN_ID%", punishment.getID());
+        if (!user.isBanned()) return;
 
-            if (punishment.getUntil() == 0) {
-                banMessage = banMessage.replace("%DURATION%", "§4§lPERMANENT!");
-            } else {
-                PrettyTime prettyTime = new PrettyTime();
-                banMessage = banMessage.replace("%DURATION%", prettyTime.format(new Date(punishment.getUntil())));
-            }
+        Punishment punishment = user.getBan();
 
-            event.disallow(PlayerLoginEvent.Result.KICK_BANNED, Component.text(banMessage));
-            for (Player p : Bukkit.getServer().getOnlinePlayers()) {
-                if (p.hasPermission(BAN_BROADCAST_PERMISSION)) {
-                    Lang.BANNED_PLAYER_TRIED_TO_JOIN.send(p, "%PLAYER%;", event.getPlayer().getName());
-                }
-            }
+        String banMessage = Lang.BAN_MESSAGE.get(event.getPlayer())
+                .replace("%REASON%", punishment.getReason())
+                .replace("%BAN_ID%", punishment.getID());
+
+        if (punishment.getUntil() == 0) {
+            banMessage = banMessage.replace("%DURATION%", "§4§lPERMANENT!");
+        } else {
+            PrettyTime prettyTime = new PrettyTime();
+            banMessage = banMessage.replace("%DURATION%", prettyTime.format(new Date(punishment.getUntil())));
         }
+
+        Lang.BANNED_PLAYER_TRIED_TO_JOIN.broadcastPermission("pixlies.moderation.logs", "%PLAYER%;", event.getPlayer().getName());
+        event.disallow(PlayerLoginEvent.Result.KICK_BANNED, Component.text(banMessage));
     }
 
 }
