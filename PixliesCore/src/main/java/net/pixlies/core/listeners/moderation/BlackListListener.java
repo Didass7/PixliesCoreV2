37a3fd78c1ff742af1a5ab4e7d212fff2208cb2b
dynmie <xbottam@gmail.com>
2022-02-11 19:04:00 -0800

    some changes to blacklist listener

--- PixliesCore/src/main/java/net/pixlies/core/listeners/moderation/BlackListListener.java
+++ PixliesCore/src/main/java/net/pixlies/core/listeners/moderation/BlackListListener.java
@@ -4,26 +4,28 @@ import net.kyori.adventure.text.Component;
 import net.pixlies.core.entity.User;
 import net.pixlies.core.localization.Lang;
 import net.pixlies.core.moderation.Punishment;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.event.player.PlayerLoginEvent;
 
 public class BlackListListener implements Listener {
 
     @EventHandler(priority = EventPriority.HIGH)
-    public void onJoin(AsyncPlayerPreLoginEvent event) {
-        User user = User.get(event.getUniqueId());
+    public void onLogin(PlayerLoginEvent event) {
+        Player player = event.getPlayer();
+        User user = User.get(player.getUniqueId());
 
         if (!user.isBlacklisted()) return;
         Punishment punishment = user.getBlacklist();
 
-        String message = Lang.BLACKLIST_MESSAGE.get()
+        String message = Lang.BLACKLIST_MESSAGE.get(player)
                 .replace("%REASON%", punishment.getReason())
                 .replace("%BAN_ID%", punishment.getID());
 
-        Lang.BANNED_PLAYER_TRIED_TO_JOIN.broadcast("pixlies.moderation.logs", "%PLAYER%;" + event.getName());
-        event.disallow(AsyncPlayerPreLoginEvent.Result.KICK_BANNED, Component.text(message));
+        Lang.BANNED_PLAYER_TRIED_TO_JOIN.broadcast("pixlies.moderation.logs", "%PLAYER%;" + player.getName());
+        event.disallow(PlayerLoginEvent.Result.KICK_BANNED, Component.text(message));
     }
 
 }
