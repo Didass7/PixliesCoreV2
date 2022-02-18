c825a012b4c4cf3e11cdc27e0d121c20445f446b
vPrototype <65795488+vyketype@users.noreply.github.com>
2022-02-18 10:17:11 -0500

    Update BypassCommand.java

--- PixliesCore/src/main/java/net/pixlies/core/commands/staff/BypassCommand.java
+++ PixliesCore/src/main/java/net/pixlies/core/commands/staff/BypassCommand.java
@@ -9,7 +9,7 @@ import org.bukkit.entity.Player;
 
 /**
  * Nation bypass command
- * @author vPrototype__
+ * @author vPrototype_
  * @author Dynmie
  */
 @CommandAlias("bypass|b|nationbypass")
@@ -17,7 +17,7 @@ import org.bukkit.entity.Player;
 public class BypassCommand extends BaseCommand {
 
     @Default
-    @Description("Toggle your territory bypass status")
+    @Description("Toggle your nation commands bypass status")
     public void onStaffMode(Player player) {
         User user = User.get(player.getUniqueId());
         if (user.getSettings().isBypassing()) {
@@ -28,13 +28,15 @@ public class BypassCommand extends BaseCommand {
     }
 
     @Private
-    @Description("Toggle another player's territory bypass status")
+    @Description("Toggle another player's nation commands bypass status")
     public void onStaffMode(CommandSender sender, Player player) {
         User user = User.get(player.getUniqueId());
         if (user.getSettings().isBypassing()) {
             Lang.STAFF_BYPASS_OFF_OTHER.send(sender, "%PLAYER%;" + player.getName());
+            Lang.STAFF_BYPASS_ON.send(player); // To alert the player that their bypass is now on
         } else {
             Lang.STAFF_BYPASS_ON_OTHER.send(sender, "%PLAYER%;" + player.getName());
+            Lang.STAFF_BYPASS_OFF.send(player); // To alert the player that their bypass is now off
         }
     }
 
