5d01d7166d5c6e345686e95813fa0923e607a09b
dynmie <xbottam@gmail.com>
2022-02-22 21:06:39 -0800

    why does the event not work

--- PixliesCore/src/main/java/net/pixlies/core/utils/EventUtils.java
+++ PixliesCore/src/main/java/net/pixlies/core/utils/EventUtils.java
@@ -2,6 +2,7 @@ package net.pixlies.core.utils;
 
 import net.pixlies.core.Main;
 import net.pixlies.core.events.PixliesCancellableEvent;
+import org.bukkit.Bukkit;
 import org.bukkit.event.Event;
 
 public final class EventUtils {
@@ -11,12 +12,22 @@ public final class EventUtils {
     private EventUtils() {}
 
     public static Event callEvent(Event event) {
-         instance.getServer().getPluginManager().callEvent(event);
-         return event;
+        if (event.isAsynchronous()) {
+            Bukkit.getScheduler().runTaskAsynchronously(instance, () ->
+                    instance.getServer().getPluginManager().callEvent(event));
+        } else {
+            instance.getServer().getPluginManager().callEvent(event);
+        }
+        return event;
     }
 
     public static PixliesCancellableEvent callCancelable(PixliesCancellableEvent event) {
-        instance.getServer().getPluginManager().callEvent(event);
+        if (event.isAsynchronous()) {
+            Bukkit.getScheduler().runTaskAsynchronously(instance, () ->
+                    instance.getServer().getPluginManager().callEvent(event));
+        } else {
+            instance.getServer().getPluginManager().callEvent(event);
+        }
         return event;
     }
 
