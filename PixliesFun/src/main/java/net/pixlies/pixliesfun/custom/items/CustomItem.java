31c852c504ef9ec7a9f1b68a937b9df63a57d31e
dynmie <xbottam@gmail.com>
2022-02-24 19:36:00 -0800

    changes to custom items

--- PixliesFun/src/main/java/net/pixlies/pixliesfun/custom/items/CustomItem.java
+++ PixliesFun/src/main/java/net/pixlies/pixliesfun/custom/items/CustomItem.java
@@ -1,13 +1,13 @@
 package net.pixlies.pixliesfun.custom.items;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
 import net.pixlies.core.Main;
 import net.pixlies.core.utils.ItemBuilder;
 import net.pixlies.pixliesfun.PixliesFun;
 import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
+import org.bukkit.event.player.PlayerDropItemEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.persistence.PersistentDataContainer;
 import org.bukkit.persistence.PersistentDataType;
@@ -19,26 +19,10 @@ import java.util.List;
  * Custom items.
  * @author Dynmie
  */
-@Data
-@AllArgsConstructor
-public class CustomItem {
+public abstract class CustomItem {
 
     private static final PixliesFun instance = PixliesFun.getInstance();
     private static final Main pixlies = Main.getInstance();
-
-    private String displayName;
-    private List<String> lore;
-    private Material material;
-    private String identifier;
-    private boolean stackable;
-
-    /**
-     * Run this when the player interacts with this item.
-     * @param event the player interact event.
-     */
-    public void onInteractItem(@NotNull PlayerInteractEvent event) {
-
-    }
 
     /**
      * Build the item into an ItemStack.
@@ -46,17 +30,74 @@ public class CustomItem {
      */
     public ItemStack build() {
 
-        ItemStack item = new ItemBuilder(material)
-                .setDisplayName(displayName)
-                .addLoreAll(lore)
+        ItemStack item = new ItemBuilder(getMaterial())
+                .setDisplayName(getDisplayName())
+                .addLoreAll(getLore())
                 .build();
 
         PersistentDataContainer container = item.getPersistentDataContainer();
         NamespacedKey key = new NamespacedKey(pixlies, "PixliesFunItem");
-        container.set(key, PersistentDataType.STRING, identifier);
+        container.set(key, PersistentDataType.STRING, getIdentifier());
 
         return item;
 
     }
+
+    /**
+     * Ran when the player interacts with this item in the main hand.
+     * Override this to change the function.
+     * @param event the player interact event.
+     */
+    public void onInteractItem(@NotNull PlayerInteractEvent event) {
+
+    }
+
+    /**
+     * Ran when the player interacts with this item in the main hand.
+     * Override this to change the function.
+     * @param event the player sneak event.
+     */
+    public void onToggleSneakItem(@NotNull PlayerToggleSneakEvent event) {
+
+    }
+
+    /**
+     * Ran when the player drops the item on the ground.
+     * Override this to change the function.
+     * @param event the item drop event.
+     */
+    public void onDropItem(@NotNull PlayerDropItemEvent event) {
+
+    }
+
+    /**
+     * Gets the display name.
+     * @return The display name of the custom item.
+     */
+    public abstract @NotNull String getDisplayName();
+
+    /**
+     * Gets the lore.
+     * @return The lore in a list.
+     */
+    public abstract @NotNull List<String> getLore();
+
+    /**
+     * Get the material.
+     * @return Get the material of the custom item.
+     */
+    public abstract @NotNull Material getMaterial();
+
+    /**
+     * Get the identifier of the custom item.
+     * @return The identifier of the custom item.
+     */
+    public abstract @NotNull String getIdentifier();
+
+    /**
+     * If the custom item is stackable.
+     * @return True if the item is stackable, false if it isn't.
+     */
+    public abstract boolean isStackable();
 
 }
