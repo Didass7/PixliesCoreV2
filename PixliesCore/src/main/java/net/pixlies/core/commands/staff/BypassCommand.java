f3db3fee4df6e281e29967a9b79c27380d37090c
dynmie <xbottam@gmail.com>
2022-02-27 15:19:23 -0800

    changes to bypass

--- PixliesCore/src/main/java/net/pixlies/core/commands/staff/BypassCommand.java
+++ PixliesCore/src/main/java/net/pixlies/core/commands/staff/BypassCommand.java
@@ -12,31 +12,41 @@ import org.bukkit.entity.Player;
  * @author vPrototype_
  * @author Dynmie
  */
-@CommandAlias("bypass|b|nationbypass")
+@CommandAlias("bypass|b")
 @CommandPermission("pixlies.moderation.bypass")
 public class BypassCommand extends BaseCommand {
 
     @Default
-    @Description("Toggle your nation commands bypass status")
-    public void onStaffMode(Player player) {
+    @Description("Toggle your staff bypass status")
+    @Subcommand("toggle")
+    public void onBypass(Player player) {
         User user = User.get(player.getUniqueId());
         if (user.getSettings().isBypassing()) {
+            user.getSettings().setBypassing(false);
+            user.save();
             Lang.STAFF_BYPASS_OFF.send(player);
         } else {
+            user.getSettings().setBypassing(true);
+            user.save();
             Lang.STAFF_BYPASS_ON.send(player);
         }
     }
 
-    @Private
-    @Description("Toggle another player's nation commands bypass status")
-    public void onStaffMode(CommandSender sender, Player player) {
+    @CommandPermission("pixlies.moderation.bypass.other")
+    @Description("Toggle another player's staff bypass bypass status")
+    @Subcommand("other")
+    public void onBypass(CommandSender sender, Player player) {
         User user = User.get(player.getUniqueId());
         if (user.getSettings().isBypassing()) {
-            Lang.STAFF_BYPASS_OFF_OTHER.send(sender, "%PLAYER%;" + player.getName());
-            Lang.STAFF_BYPASS_ON.send(player); // To alert the player that their bypass is now on
-        } else {
-            Lang.STAFF_BYPASS_ON_OTHER.send(sender, "%PLAYER%;" + player.getName());
+            user.getSettings().setBypassing(false);
+            user.save();
             Lang.STAFF_BYPASS_OFF.send(player); // To alert the player that their bypass is now off
+            Lang.STAFF_BYPASS_OFF_OTHER.send(sender, "%PLAYER%;" + player.getName());
+        } else {
+            user.getSettings().setBypassing(true);
+            user.save();
+            Lang.STAFF_BYPASS_ON.send(player); // To alert the player that their bypass is now on
+            Lang.STAFF_BYPASS_ON_OTHER.send(sender, "%PLAYER%;" + player.getName());
         }
     }
 
