8445a28dc837c359f4022a109a6b14a63e4a2858
vPrototype <65795488+vyketype@users.noreply.github.com>
2022-03-03 14:19:53 -0500

    Update OrderItem.java

--- ProtoBusiness/src/main/java/net/pixlies/business/market/OrderItem.java
+++ ProtoBusiness/src/main/java/net/pixlies/business/market/OrderItem.java
@@ -14,6 +14,8 @@ import java.util.List;
 @AllArgsConstructor
 public enum OrderItem {
 
+    // TODO: TERRACOTTA, WHITE_STAINED_GLASS, WHITE_STAINED_GLASS_PANE, WHITE_WOOL, SHULKER_BOX
+
     // MINERALS
 
     COBBLESTONE(Material.COBBLESTONE, 0, 0, 0),
@@ -77,13 +79,91 @@ public enum OrderItem {
     COOKED_SALMON(Material.NETHERITE_SCRAP, 1, 3, 4),
     SUGAR_CANE(Material.SUGAR_CANE, 1, 4, 4),
     CACTUS(Material.CACTUS, 1, 5, 4),
-    LILY_PAD(Material.LILY_PAD, 1, 6, 4);
+    LILY_PAD(Material.LILY_PAD, 1, 6, 4),
 
     // BLOCKS
 
+    DIRT(Material.DIRT, 1, 0, 0),
+    GRASS_BLOCK(Material.GRASS_BLOCK, 1, 1, 0),
+    MOSS_BLOCK(Material.MOSS_BLOCK, 1, 2, 0),
+    MYCELIUM(Material.MYCELIUM, 1, 3, 0),
+    SAND(Material.SAND, 1, 4, 0),
+    RED_SAND(Material.RED_SAND, 1, 5, 0),
+    GLASS(Material.GLASS, 1, 6, 0),
+    OAK_LOG(Material.OAK_LOG, 1, 0, 1),
+    SPRUCE_LOG(Material.SPRUCE_LOG, 1, 1, 1),
+    BIRCH_LOG(Material.BIRCH_LOG, 1, 2, 1),
+    JUNGLE_LOG(Material.JUNGLE_LOG, 1, 3, 1),
+    ACACIA_LOG(Material.ACACIA_LOG, 1, 4, 1),
+    DARK_OAK_LOG(Material.DARK_OAK_LOG, 1, 5, 1),
+    GLASS_PANE(Material.GLASS_PANE, 1, 6, 1),
+    CRIMSON_STEM(Material.CRIMSON_STEM, 1, 0, 2),
+    WARPED_STEM(Material.WARPED_STEM, 1, 1, 2),
+    NETHERRACK(Material.NETHERRACK, 1, 2, 2),
+    SOUL_SAND(Material.SOUL_SAND, 1, 3, 2),
+    SOUL_SOIL(Material.SOUL_SOIL, 1, 4, 2),
+    BASALT(Material.BASALT, 1, 5, 2),
+    WHITE_STAINED_GLASS(Material.WHITE_STAINED_GLASS, 1, 6, 2),
+    BLACKSTONE(Material.BLACKSTONE, 1, 0, 3),
+    MAGMA_BLOCK(Material.MAGMA_BLOCK, 1, 1, 3),
+    SHROOMLIGHT(Material.SHROOMLIGHT, 1, 2, 3),
+    TERRACOTTA(Material.TERRACOTTA, 1, 3, 3),
+    END_STONE(Material.END_STONE, 1, 4, 3),
+    GRAVEL(Material.GRAVEL, 1, 5, 3),
+    WHITE_STAINED_GLASS_PANE(Material.WHITE_STAINED_GLASS_PANE, 1, 6, 3),
+    SNOW_BLOCK(Material.SNOW_BLOCK, 1, 0, 4),
+    ICE(Material.ICE, 1, 1, 4),
+    PACKED_ICE(Material.PACKED_ICE, 1, 2, 4),
+    BLUE_ICE(Material.BLUE_ICE, 1, 3, 4),
+    PRISMARINE(Material.PRISMARINE, 1, 4, 4),
+    SEA_LANTERN(Material.SEA_LANTERN, 1, 5, 4),
+    TINTED_GLASS(Material.TINTED_GLASS, 1, 6, 4),
+
     // MOB DROPS
 
+    ROTTEN_FLESH(Material.ROTTEN_FLESH, 0, 0, 0),
+    STRING(Material.STRING, 0, 1, 0),
+    SPIDER_EYE(Material.SPIDER_EYE, 0, 2, 0),
+    GUNPOWDER(Material.GUNPOWDER, 0, 3, 0),
+    BONE(Material.BONE, 0, 4, 0),
+    SLIME_BALL(Material.SLIME_BALL, 0, 5, 0),
+    MAGMA_CREAM(Material.MAGMA_CREAM, 0, 0, 1),
+    BLAZE_ROD(Material.BLAZE_ROD, 0, 1, 1),
+    GHAST_TEAR(Material.GHAST_TEAR, 0, 2, 1),
+    ENDER_PEARL(Material.ENDER_PEARL, 0, 3, 1),
+    DRAGON_BREATH(Material.DRAGON_BREATH, 0, 4, 1),
+    PHANTOM_MEMBRANE(Material.PHANTOM_MEMBRANE, 0, 5, 1),
+    INK_SAC(Material.INK_SAC, 0, 0, 2),
+    GLOW_INK_SAC(Material.GLOW_INK_SAC, 0, 1, 2),
+    LEATHER(Material.LEATHER, 0, 2, 2),
+    RABBIT_HIDE(Material.RABBIT_HIDE, 0, 3, 2),
+    RABBIT_FOOT(Material.RABBIT_FOOT, 0, 4, 2),
+    FEATHER(Material.FEATHER, 0, 5, 2),
+    WHITE_WOOL(Material.WHITE_WOOL, 0, 0, 3),
+    BONE_BLOCK(Material.BONE_BLOCK, 0, 1, 3),
+    SHULKER_SHELL(Material.SHULKER_SHELL, 0, 2, 3),
+    SHULKER_BOX(Material.SHULKER_BOX, 0, 3, 3),
+
     // MISCELLANEOUS
+
+    FLINT(Material.FLINT, 0, 0, 0),
+    SNOWBALL(Material.SNOWBALL, 0, 1, 0),
+    CLAY_BALL(Material.CLAY_BALL, 0, 2, 0),
+    ARROW(Material.ARROW, 0, 3, 0),
+    NETHER_WART(Material.NETHER_WART, 0, 4, 0),
+    EXPERIENCE_BOTTLE(Material.EXPERIENCE_BOTTLE, 0, 5, 0),
+    PRISMARINE_SHARD(Material.PRISMARINE_SHARD, 0, 0, 1),
+    PRISMARINE_CRYSTALS(Material.PRISMARINE_CRYSTALS, 0, 1, 1),
+    NAUTILUS_SHELL(Material.NAUTILUS_SHELL, 0, 2, 1),
+    HEART_OF_THE_SEA(Material.HEART_OF_THE_SEA, 0, 3, 1),
+    SPONGE(Material.SPONGE, 0, 4, 1),
+    NAME_TAG(Material.NAME_TAG, 0, 5, 1),
+    CHORUS_FRUIT(Material.CHORUS_FRUIT, 0, 0, 2),
+    CHORUS_FLOWER(Material.CHORUS_FLOWER, 0, 1, 2),
+    SCUTE(Material.SCUTE, 0, 2, 2),
+    LEAD(Material.LEAD, 0, 3, 2),
+    TURTLE_EGG(Material.TURTLE_EGG, 0, 4, 2),
+    NETHER_STAR(Material.NETHER_STAR, 0, 5, 2);
 
     /**
      * Material of the item
