f168ebb39dc4ec467c41b9d0fd40dd7747a794f6
dynmie <xbottam@gmail.com>
2022-03-09 17:55:53 -0800

    can withdraw

--- PixliesCore/src/main/java/net/pixlies/core/economy/Wallet.java
+++ PixliesCore/src/main/java/net/pixlies/core/economy/Wallet.java
@@ -25,14 +25,23 @@ public class Wallet {
 
     public void deposit(double amount, String reason) {
         this.balance += amount;
-        history.add("+;" + amount + ";" + reason + ";" + System.currentTimeMillis());
+        history.add("+;" + amount + ";" + reason.replace(";", ":") + ";" + System.currentTimeMillis());
     }
 
     public boolean withdraw(double amount, String reason) {
         if (this.balance - amount < 0) return false;
         this.balance -= amount;
-        this.history.add("-;" + amount + ";" + reason + ";" + System.currentTimeMillis());
+        this.history.add("-;" + amount + ";" + reason.replace(";", ":") + ";" + System.currentTimeMillis());
         return true;
+    }
+
+    /**
+     * Check if the player is able to withdraw a certain amount of money
+     * @param amount The amount of cash to check
+     * @return True of the player can withdraw; False if the player cannot withdraw.
+     */
+    public boolean canWithdraw(double amount) {
+        return !(this.balance - amount < 0);
     }
 
     public String format(double amount) {
