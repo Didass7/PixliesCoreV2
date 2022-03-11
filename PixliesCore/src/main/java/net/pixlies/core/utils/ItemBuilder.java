41fb60e6435627105099892ec8cf05275886cd35
dynmie <xbottam@gmail.com>
2022-03-10 18:22:09 -0800

    i forgot to override so i did it

--- PixliesCore/src/main/java/net/pixlies/core/utils/ItemBuilder.java
+++ PixliesCore/src/main/java/net/pixlies/core/utils/ItemBuilder.java
@@ -176,6 +176,7 @@ public class ItemBuilder {
      * @return The copied ItemBuilder
      */
     @SneakyThrows
+    @Override
     public @NotNull ItemBuilder clone() {
         return (ItemBuilder) super.clone();
     }
