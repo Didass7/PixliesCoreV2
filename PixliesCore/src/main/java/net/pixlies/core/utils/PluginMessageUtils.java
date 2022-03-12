6fb05d55c77a27c9b8da6b52a4c024ceba713ca6
dynmie <xbottam@gmail.com>
2022-03-11 22:10:24 -0800

    cool little class

---
+++ PixliesCore/src/main/java/net/pixlies/core/utils/PluginMessageUtils.java
@@ -0,0 +1,38 @@
+package net.pixlies.core.utils;
+
+import net.pixlies.core.Main;
+import org.bukkit.entity.Player;
+
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+public final class PluginMessageUtils {
+
+    private PluginMessageUtils() {}
+
+    public static void sendMessage(Player player, String channel, DataOutput output) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        DataOutputStream out = new DataOutputStream(stream);
+
+        try {
+            output.execute(out);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        player.sendPluginMessage(Main.getInstance(), channel, stream.toByteArray());
+
+    }
+
+    public interface DataOutput {
+
+        /**
+         * Stream is caught with IOException
+         * @param stream the stream
+         */
+        void execute(DataOutputStream stream) throws IOException;
+
+    }
+
+}
