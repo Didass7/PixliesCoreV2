09558374160cb0b0694fd26d284edcd51d9eb195
MickMMars <a.khamisipour@outlook.de>
2022-03-16 12:37:23 +0100

    + Started /world create <name> <type> <environment>

--- PixliesCore/src/main/java/net/pixlies/core/commands/staff/WorldCommand.java
+++ PixliesCore/src/main/java/net/pixlies/core/commands/staff/WorldCommand.java
@@ -7,6 +7,8 @@ import net.pixlies.core.localization.Lang;
 import net.pixlies.core.utils.TextUtils;
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.WorldCreator;
+import org.bukkit.WorldType;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
@@ -35,6 +37,21 @@ public class WorldCommand extends BaseCommand {
         player.teleport(location);
         Lang.STAFF_TELEPORT_PLAYER_TO_TARGET.send(sender, "%PLAYER%;" + player.getName(),
                 "%TARGET%;" + TextUtils.getLocationFormatted(location));
+    }
+
+    @Subcommand("create")
+    @CommandPermission("pixlies.staff.world.create")
+    public void onCreate(CommandSender sender, String name, String type, String environment) {
+        WorldCreator wc = new WorldCreator(name);
+
+        //TODO: Nullchecks N' Stuffs
+        wc.environment(World.Environment.valueOf(environment));
+        wc.type(WorldType.valueOf(type));
+
+        World world = wc.createWorld();
+
+        if (sender instanceof Player player) player.teleport(world.getSpawnLocation());
+        //TODO: Send creation message
     }
 
     @HelpCommand
