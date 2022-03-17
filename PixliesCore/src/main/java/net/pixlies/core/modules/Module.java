a9fabdb5cd3f13a84722fb504af11c1b849322b7
dynmie <xbottam@gmail.com>
2022-03-17 14:50:16 -0700

    i think this will fix the module resource

--- PixliesCore/src/main/java/net/pixlies/core/modules/Module.java
+++ PixliesCore/src/main/java/net/pixlies/core/modules/Module.java
@@ -3,9 +3,13 @@ package net.pixlies.core.modules;
 import lombok.Getter;
 import net.pixlies.core.Main;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.logging.Logger;
 
 public abstract class Module {
@@ -18,15 +22,31 @@ public abstract class Module {
         this.description = description;
         moduleFolder = new File(Main.getInstance().getDataFolder().getAbsolutePath() + File.separator + "modules" + File.separator + description.getName());
         logger = Logger.getLogger(description.getName());
+    }
+
+    public InputStream getResource(@NotNull String string) {
+        try {
+            URL url = this.getClass().getClassLoader().getResource(string);
+
+            if (url == null) {
+                return null;
+            } else {
+                URLConnection connection = url.openConnection();
+                connection.setUseCaches(false);
+                return connection.getInputStream();
+            }
+
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public void onLoad() {
 
     }
 
-    public InputStream getResource(String string) {
-        return this.getClass().getClassLoader().getResourceAsStream(string);
+    public void onDrop() {
+
     }
-
-    public abstract void onLoad();
-
-    public abstract void onDrop();
 
 }
