490a0d9f40eceb82c23c1280195a1e669abc7160
dynmie <xbottam@gmail.com>
2022-07-26 20:17:54 -0700

    kickall confirmation

--- PixliesCore/src/main/java/net/pixlies/core/commands/moderation/KickallCommand.java
+++ PixliesCore/src/main/java/net/pixlies/core/commands/moderation/KickallCommand.java
@@ -5,6 +5,7 @@ import co.aikar.commands.annotation.*;
 import net.kyori.adventure.text.Component;
 import net.pixlies.core.Main;
 import net.pixlies.core.localization.Lang;
+import net.pixlies.core.utils.CC;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -15,10 +16,17 @@ public class KickallCommand extends BaseCommand {
 
     private static final Main instance = Main.getInstance();
 
+    @Default
     @Description("Kick all players")
     @CommandCompletion("@empty")
-    public void onKickAll(CommandSender sender, @Optional String reason) {
+    public void onKickAll(CommandSender sender) {
+        sender.sendMessage(CC.format(Lang.PIXLIES + "&7Are you sure you want to kick all players? Type \"&c/kickall confirm [reason]\"&7 to confirm."));
+    }
 
+    @Private
+    @Subcommand("confirm")
+    @Syntax("[reason]")
+    public void onConfirm(CommandSender sender, @Optional String reason) {
         String newReason = reason == null ? instance.getConfig().getString("moderation.defaultReason", "No reason given") : reason;
 
         for (Player player : Bukkit.getOnlinePlayers()) {
