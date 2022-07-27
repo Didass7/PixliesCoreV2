0ec883fe4cabc2f5e7f62db279e628481898c9f7
dynmie <xbottam@gmail.com>
2022-07-26 20:07:49 -0700

    finally fixed broadcast, i think it was @Default

--- PixliesCore/src/main/java/net/pixlies/core/commands/staff/BroadcastCommand.java
+++ PixliesCore/src/main/java/net/pixlies/core/commands/staff/BroadcastCommand.java
@@ -1,13 +1,12 @@
 package net.pixlies.core.commands.staff;
 
 import co.aikar.commands.BaseCommand;
-import co.aikar.commands.annotation.CommandAlias;
-import co.aikar.commands.annotation.CommandPermission;
-import co.aikar.commands.annotation.Default;
-import co.aikar.commands.annotation.Description;
+import co.aikar.commands.CommandHelp;
+import co.aikar.commands.annotation.*;
 import net.pixlies.core.localization.Lang;
 import net.pixlies.core.utils.CC;
 import org.bukkit.command.CommandSender;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Broadcast command to broadcast
@@ -16,13 +15,19 @@ import org.bukkit.command.CommandSender;
 @CommandPermission("pixlies.staff.broadcast")
 public class BroadcastCommand extends BaseCommand {
 
-    @Default
     @CommandAlias("broadcast|bc")
+    @CommandCompletion("@empty")
+    @Syntax("<message>")
     @Description("Broadcast a message")
-    public void onBroadcast(CommandSender sender, String text) {
-        // if player has perm below then allow color, else normal text
-        String message = sender.hasPermission("pixlies.staff.broadcast.color") ? CC.format(text) : text;
-        Lang.STAFF_BROADCAST_MESSAGE.broadcast("%MESSAGE%;" + message);
+    public void onBroadcast(CommandSender sender, String message) {
+        // if player has perm below then allow color, else normal message
+        String msg = sender.hasPermission("pixlies.staff.broadcast.color") ? CC.format(message) : message;
+        Lang.STAFF_BROADCAST_MESSAGE.broadcast("%MESSAGE%;" + msg.trim());
+    }
+
+    @Default
+    public void onHelp(CommandHelp help) {
+        help.showHelp();
     }
 
 }
