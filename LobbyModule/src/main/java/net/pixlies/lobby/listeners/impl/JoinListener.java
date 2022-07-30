5b8cccf4027f1f74da7b74729f088926a4e5ecb5
dynmie <xbottam@gmail.com>
2022-07-29 18:25:57 -0700

    join messages

--- LobbyModule/src/main/java/net/pixlies/lobby/listeners/impl/JoinListener.java
+++ LobbyModule/src/main/java/net/pixlies/lobby/listeners/impl/JoinListener.java
@@ -1,5 +1,9 @@
 package net.pixlies.lobby.listeners.impl;
 
+import me.clip.placeholderapi.PlaceholderAPI;
+import net.pixlies.core.modules.configuration.ModuleConfig;
+import net.pixlies.core.utils.CC;
+import net.pixlies.lobby.Lobby;
 import net.pixlies.lobby.utils.LobbyUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,10 +12,19 @@ import org.bukkit.event.player.PlayerJoinEvent;
 
 public class JoinListener implements Listener {
 
+    private static final Lobby instance = Lobby.getInstance();
+    private static final ModuleConfig config = instance.getConfig();
+
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
 
         Player player = event.getPlayer();
+
+        String joinMessage = CC.format(PlaceholderAPI.setPlaceholders(player, config.getString("join_leave.join_message", "")));
+        if (!joinMessage.isEmpty()) {
+            instance.getServer().getScheduler().runTaskLater(instance, () -> player.sendMessage(joinMessage), 5);
+        }
+
         LobbyUtils.resetPlayer(player, true);
 
     }
