decdc1848d9e414d862f00b7a16db83d2afb1c21
dynmie <xbottam@gmail.com>
2022-08-02 22:12:24 -0700

    teleportation timer cancel on hit or hurt

--- PixliesCore/src/main/java/net/pixlies/core/listeners/player/TeleportListener.java
+++ PixliesCore/src/main/java/net/pixlies/core/listeners/player/TeleportListener.java
@@ -7,7 +7,10 @@ import net.pixlies.core.localization.Lang;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -57,6 +60,48 @@ public class TeleportListener implements Listener {
         if (!from.getBlock().equals(to.getBlock())) {
             user.cancelTeleport();
             Lang.TP_CANCEL.send(player);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onHurt(EntityDamageEvent event) {
+        if (event.isCancelled()) return;
+
+        if (!(event.getEntity() instanceof Player player)) return;
+        User user = User.get(player.getUniqueId());
+
+        if (!user.isTeleporting()) {
+            return;
+        }
+
+        user.cancelTeleport();
+        Lang.TP_CANCEL.send(player);
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onHurtByEntity(EntityDamageByEntityEvent event) {
+        if (event.isCancelled()) return;
+
+        if (event.getEntity() instanceof Player victim) {
+            User user = User.get(victim.getUniqueId());
+
+            if (!user.isTeleporting()) {
+                return;
+            }
+
+            user.cancelTeleport();
+            Lang.TP_CANCEL.send(victim);
+        }
+
+        if (event.getDamager() instanceof Player damager) {
+            User user = User.get(damager.getUniqueId());
+
+            if (!user.isTeleporting()) {
+                return;
+            }
+
+            user.cancelTeleport();
+            Lang.TP_CANCEL.send(damager);
         }
 
     }
