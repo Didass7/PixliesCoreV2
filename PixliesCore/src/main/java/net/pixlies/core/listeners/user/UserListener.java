c2926ce1601dc3c3d84ec8abea9400fa0c8b68eb
dynmie <xbottam@gmail.com>
2022-08-14 18:58:17 -0700

    fix remove player from cache on join to get new info thing

--- PixliesCore/src/main/java/net/pixlies/core/listeners/user/UserListener.java
+++ PixliesCore/src/main/java/net/pixlies/core/listeners/user/UserListener.java
@@ -3,11 +3,10 @@ package net.pixlies.core.listeners.user;
 import lombok.val;
 import net.pixlies.core.Main;
 import net.pixlies.core.entity.user.User;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 public class UserListener implements Listener {
@@ -15,10 +14,8 @@ public class UserListener implements Listener {
     private final Main instance = Main.getInstance();
 
     @EventHandler(priority = EventPriority.HIGHEST)
-    public void onJoin(PlayerJoinEvent event) {
-        Player player = event.getPlayer();
-        instance.getDatabase().getUserCache().remove(player.getUniqueId());
-        User.get(player.getUniqueId()); // load the user
+    public void onPreLogin(AsyncPlayerPreLoginEvent event) {
+        instance.getDatabase().getUserCache().remove(event.getUniqueId());
     }
 
     @EventHandler(priority = EventPriority.HIGHEST)
