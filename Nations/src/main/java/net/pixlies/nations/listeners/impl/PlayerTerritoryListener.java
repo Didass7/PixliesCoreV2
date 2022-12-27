433f975549110544d8cfa88214db30f0d373064b
Dynmie <41315732+dynmie@users.noreply.github.com>
2022-12-26 18:15:08 -0800

    some bug fixes

--- Nations/src/main/java/net/pixlies/nations/listeners/impl/PlayerTerritoryListener.java
+++ Nations/src/main/java/net/pixlies/nations/listeners/impl/PlayerTerritoryListener.java
@@ -5,8 +5,13 @@ import net.pixlies.nations.events.impl.PlayerTerritoryChangeEvent;
 import net.pixlies.nations.nations.chunk.NationChunk;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerChangedWorldEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+import org.bukkit.event.player.PlayerTeleportEvent;
 
 import java.util.Objects;
 
@@ -21,15 +26,45 @@ public class PlayerTerritoryListener implements Listener {
 
         if (Objects.equals(from, to)) return;
 
-        // TODO: Territory Change event call & handle
-        if (from == null) from = new NationChunk("WILDERNESS", event.getFrom().getWorld().getName(), event.getFrom().getChunk().getX(), event.getFrom().getChunk().getZ());
-        if (to == null) to = new NationChunk("WILDERNESS", event.getTo().getWorld().getName(), event.getTo().getChunk().getX(), event.getTo().getChunk().getZ());
+        if (from == null) from = new NationChunk("wilderness", event.getFrom().getWorld().getName(), event.getFrom().getChunk().getX(), event.getFrom().getChunk().getZ());
+        if (to == null) to = new NationChunk("wilderness", event.getTo().getWorld().getName(), event.getTo().getChunk().getX(), event.getTo().getChunk().getZ());
         if (from.getNationId().equals(to.getNationId())) return;
 
         PlayerTerritoryChangeEvent playerTerritoryChangeEvent = new PlayerTerritoryChangeEvent(player, from, to);
         EventUtils.call(playerTerritoryChangeEvent);
 
         event.setCancelled(playerTerritoryChangeEvent.isCancelled());
+    }
+
+    @EventHandler
+    public void onPlayerTeleport(PlayerTeleportEvent event) {
+        Player player = event.getPlayer();
+
+        NationChunk from = NationChunk.getClaimFromChunk(event.getFrom().getChunk());
+        NationChunk to = NationChunk.getClaimFromChunk(event.getTo().getChunk());
+
+        if (Objects.equals(from, to)) return;
+
+        if (from == null) from = new NationChunk("wilderness", event.getFrom().getWorld().getName(), event.getFrom().getChunk().getX(), event.getFrom().getChunk().getZ());
+        if (to == null) to = new NationChunk("wilderness", event.getTo().getWorld().getName(), event.getTo().getChunk().getX(), event.getTo().getChunk().getZ());
+        if (from.getNationId().equals(to.getNationId())) return;
+
+        PlayerTerritoryChangeEvent playerTerritoryChangeEvent = new PlayerTerritoryChangeEvent(player, from, to);
+        EventUtils.call(playerTerritoryChangeEvent);
+
+        event.setCancelled(playerTerritoryChangeEvent.isCancelled());
+    }
+
+    @EventHandler(priority = EventPriority.LOWEST)
+    public void onDeath(PlayerRespawnEvent event) {
+        Player player = event.getPlayer();
+
+        NationChunk to = NationChunk.getClaimFromChunk(event.getRespawnLocation().getChunk());
+
+        if (to == null) to = new NationChunk("wilderness", event.getRespawnLocation().getWorld().getName(), event.getRespawnLocation().getChunk().getX(), event.getRespawnLocation().getChunk().getZ());
+
+        PlayerTerritoryChangeEvent playerTerritoryChangeEvent = new PlayerTerritoryChangeEvent(player, null, to);
+        EventUtils.call(playerTerritoryChangeEvent);
     }
 
 }
