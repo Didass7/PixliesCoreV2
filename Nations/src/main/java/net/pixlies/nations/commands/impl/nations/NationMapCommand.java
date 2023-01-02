5feb212aaaa55bae016a88f3759d749bd4c2f8b1
Dynmie <41315732+dynmie@users.noreply.github.com>
2023-01-01 17:31:31 -0800

    nation map fix

--- Nations/src/main/java/net/pixlies/nations/commands/impl/nations/NationMapCommand.java
+++ Nations/src/main/java/net/pixlies/nations/commands/impl/nations/NationMapCommand.java
@@ -6,15 +6,19 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.event.HoverEvent;
 import net.pixlies.nations.Nations;
+import net.pixlies.nations.locale.NationsLang;
 import net.pixlies.nations.nations.Nation;
 import net.pixlies.nations.nations.chunk.NationChunk;
 import net.pixlies.nations.nations.interfaces.NationProfile;
+import net.pixlies.nations.nations.relations.Relation;
+import net.pixlies.nations.utils.NationUtils;
 import org.bukkit.World;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.StringJoiner;
 
 @CommandAlias("%nationscommand")
 @Subcommand("map")
@@ -27,63 +31,78 @@ public class NationMapCommand extends BaseCommand {
     // -------------------------------------------------------------------------------------------------
     @Subcommand("chat")
     @Description("Display nearby chunks and their occupation in chat")
-    public void onChat(CommandSender sender) {
-        renderChatMap((Player) sender);
+    public void onChat(Player player) {
+        renderChatMap(player);
     }
 
     public void renderChatMap(Player player) {
+        // NationProfile is never null, check if the player is in a nation
         final long start = System.currentTimeMillis();
         NationProfile profile = NationProfile.get(player.getUniqueId());
         List<TextComponent> rows = new ArrayList<>();
         final int height = 6;
         final int width = (height * 2);
 
-        final int playerCX = player.getLocation().getChunk().getX();
-        final int playerCZ = player.getLocation().getChunk().getZ();
+        final int playerChunkX = player.getLocation().getChunk().getX();
+        final int playerChunkZ = player.getLocation().getChunk().getZ();
         final World world = player.getWorld();
+
+        // for each row
         for (int row = height; row >= -height; row--) {
             TextComponent.Builder comp = Component.text();
+            // for each column
             for (int x = width; x >= -width; x--) {
-                final int chunkX = playerCX - x,
-                        chunkZ = playerCZ - row;
-                NationChunk nc = NationChunk.getClaimAt(world.getName(), chunkX, chunkZ);
-                if (chunkX == playerCX && chunkZ == playerCZ) {
-                    if (nc == null) {
-                        TextComponent cComp = Component.text().content("§2█").hoverEvent(HoverEvent.showText(Component.text().content("§cWilderness\n§7You may claim here.").build())).build();
-                        comp.append(cComp);
-                    } else {
-                        Nation nation = nc.getNation();
-                        TextComponent cComp = Component.text().content("§e█").hoverEvent(HoverEvent.showText(Component.text().content("§f" + nation.getName() + "\n" + "§7" + nation.getDescription()).build())).build();
-                        comp.append(cComp);
-                    }
-                } else {
-                    if (profile.isInNation()) {
-                        if (nc == null) {
-                            TextComponent cComp = Component.text().content("§2█").hoverEvent(HoverEvent.showText(Component.text().content("§cWilderness\n§7You may claim here.").build())).build();
-                            comp.append(cComp);
-                        } else {
-                            String colChar = profile.getNation().getRelationTo(nc.getNation()).getColor();
-                            Nation nation = nc.getNation();
-                            TextComponent cComp = Component.text().content(colChar + "█").hoverEvent(HoverEvent.showText(Component.text().content(colChar + nation.getName() + "\n" + "§7" + nation.getDescription()).build())).build();
-                            comp.append(cComp);
-                        }
-                    } else {
-                        if (nc == null) {
-                            TextComponent cComp = Component.text().content("§2█").hoverEvent(HoverEvent.showText(Component.text().content("§cWilderness\n§7You may claim here.").build())).build();
-                            comp.append(cComp);
-                        } else {
-                            Nation nation = nc.getNation();
-                            TextComponent cComp = Component.text().content("§e█").hoverEvent(HoverEvent.showText(Component.text().content("§f" + nation.getName() + "\n" + "§7" + nation.getDescription()).build())).build();
-                            comp.append(cComp);
-                        }
-                    }
+
+                final int chunkX = playerChunkX - x;
+                final int chunkZ = playerChunkZ - row;
+                boolean playerChunk = chunkX == playerChunkX && chunkZ == playerChunkZ;
+
+                NationChunk nChunk = NationChunk.getClaimAt(world.getName(), chunkX, chunkZ);
+                if (nChunk == null) {
+                    TextComponent cComp = Component.text()
+                            .content(playerChunk ? "§7█" : "§2█")
+                            .hoverEvent(HoverEvent.showText(Component.text()
+                                    .content("§cWilderness\n§7You may claim here.")
+                                    .build()))
+                            .build();
+                    comp.append(cComp);
+
+                    continue;
                 }
+
+                Nation nation = nChunk.getNation();
+                if (nation == null) {
+                    TextComponent cComp = Component.text()
+                            .content(playerChunk ? "§7█" : "§2█")
+                            .hoverEvent(HoverEvent.showText(Component.text()
+                                    .content("§cWilderness\n§7You may claim here.")
+                                    .build()))
+                            .build();
+                    comp.append(cComp);
+
+                    continue;
+                }
+
+                String colorNationName = NationUtils.getColorNameFromNationChunk(player, nChunk);
+                String colorNationDesc = NationUtils.getColorDescFromNationChunk(player, nChunk);
+
+                String relationColor = NationUtils.getRelationColorFromNationChunk(player, nChunk);
+
+                TextComponent cComp = Component.text()
+                        .content(playerChunk ? "§7█" : relationColor + "█")
+                        .hoverEvent(HoverEvent.showText(Component.text()
+                                .content(colorNationName + "\n" + NationsLang.NATION_SUBTITLE_COLOUR.get() + colorNationDesc)
+                                .build()))
+                        .build();
+                comp.append(cComp);
+
             }
             rows.add(comp.build());
         }
+
         for (TextComponent r : rows)
             player.sendMessage(r);
-        player.sendMessage("§7Legend: §b█You §8| §b█Yours §8| §d█Ally §8| §2█Wilderness");
+        player.sendMessage("§7Legend: §a█Nation §8| §b█Neutral §8| §d█Ally §8| §2█Wilderness");
     }
 
 }
--- Nations/src/main/java/net/pixlies/nations/utils/NationUtils.java
+++ Nations/src/main/java/net/pixlies/nations/utils/NationUtils.java
@@ -1,5 +1,15 @@
 package net.pixlies.nations.utils;
 
+import net.pixlies.core.utils.CC;
+import net.pixlies.nations.locale.NationsLang;
+import net.pixlies.nations.nations.Nation;
+import net.pixlies.nations.nations.chunk.NationChunk;
+import net.pixlies.nations.nations.interfaces.NationProfile;
+import net.pixlies.nations.nations.ranks.NationPermission;
+import net.pixlies.nations.nations.relations.Relation;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.Nullable;
+
 import java.util.Random;
 
 /**
@@ -7,7 +17,7 @@ import java.util.Random;
  *
  * @author MickMMars
  */
-public class NationUtils {
+public final class NationUtils {
 
     public static String randomDescription() {
         String[] arr = new String[] {
@@ -28,6 +38,90 @@ public class NationUtils {
         if (name.length() > 16 || name.length() < 4) return false;
         if (!name.matches("^[a-zA-Z0-9]*$")) return false;
         return !name.equalsIgnoreCase("confirm");
+    }
+
+    public static String getColorNameFromNationChunk(Player player, @Nullable NationChunk nationChunk) {
+        if (nationChunk == null) {
+            return NationsLang.NATION_WILDERNESS_TITLE.get(player);
+        }
+
+        NationProfile profile = NationProfile.get(player.getUniqueId());
+        switch (nationChunk.getNationId()) {
+            case "spawn" -> {
+                return NationsLang.NATION_SPAWN_TITLE.get(player);
+            }
+            case "warp" -> {
+                return NationsLang.NATION_WARP_TITLE.get(player);
+            }
+            case "warzone" -> {
+                return NationsLang.NATION_WARZONE_TITLE.get(player);
+            }
+            default -> {
+                Nation nation = nationChunk.getNation();
+                if (nation == null) {
+                    return NationsLang.NATION_WILDERNESS_TITLE.get(player);
+                }
+
+                Relation relation = profile.getRelationTo(nation);
+
+                return relation.getColor() + nation.getName();
+            }
+        }
+    }
+
+    public static String getColorDescFromNationChunk(Player player, @Nullable NationChunk nationChunk) {
+        if (nationChunk == null) {
+            return NationsLang.NATION_WILDERNESS_SUBTITLE.get(player);
+        }
+
+        switch (nationChunk.getNationId()) {
+            case "spawn" -> {
+                return NationsLang.NATION_SPAWN_SUBTITLE.get(player);
+            }
+            case "warp" -> {
+                return NationsLang.NATION_WARP_SUBTITLE.get(player);
+            }
+            case "warzone" -> {
+                return NationsLang.NATION_WARZONE_SUBTITLE.get(player);
+            }
+            default -> {
+                Nation nation = nationChunk.getNation();
+                if (nation == null) {
+                    return NationsLang.NATION_WILDERNESS_SUBTITLE.get(player);
+                }
+
+                return nation.getDescription();
+            }
+        }
+    }
+
+    public static String getRelationColorFromNationChunk(Player player, @Nullable NationChunk nationChunk) {
+        if (nationChunk == null) {
+            return NationsLang.NATION_WILDERNESS_COLOR.get(player);
+        }
+
+        NationProfile profile = NationProfile.get(player.getUniqueId());
+        switch (nationChunk.getNationId()) {
+            case "spawn" -> {
+                return NationsLang.NATION_SPAWN_COLOR.get(player);
+            }
+            case "warp" -> {
+                return NationsLang.NATION_WARP_COLOR.get(player);
+            }
+            case "warzone" -> {
+                return NationsLang.NATION_WARZONE_COLOR.get(player);
+            }
+            default -> {
+                Nation nation = nationChunk.getNation();
+                if (nation == null) {
+                    return NationsLang.NATION_WILDERNESS_COLOR.get(player);
+                }
+
+                Relation relation = profile.getRelationTo(nation);
+
+                return relation.getColor();
+            }
+        }
     }
 
 }
