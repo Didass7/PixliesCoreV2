b6746a4b9dfedf0711c9e82d9e6b6b8c827990cb
vPrototype <65795488+vyketype@users.noreply.github.com>
2023-01-02 17:40:09 -0500

    Lobby: Disable debate title

--- LobbyModule/src/main/java/net/pixlies/lobby/listeners/impl/JoinListener.java
+++ LobbyModule/src/main/java/net/pixlies/lobby/listeners/impl/JoinListener.java
@@ -5,7 +5,6 @@ import net.pixlies.core.modules.configuration.ModuleConfig;
 import net.pixlies.core.utils.CC;
 import net.pixlies.lobby.Lobby;
 import net.pixlies.lobby.utils.LobbyUtils;
-import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -21,10 +20,14 @@ public class JoinListener implements Listener {
 
         Player player = event.getPlayer();
     
+        /* DISABLED FOR NOW -- ONLY TO BE USED DURING COMMUNITY PRESIDENT DEBATE TIMES
+        
         instance.getServer().getScheduler().runTaskLater(instance, () -> {
             player.sendTitle("§a§l/debate", "§7Run §a/debate §7to view the debate!");
             player.playSound(player.getLocation(), Sound.UI_TOAST_CHALLENGE_COMPLETE, 100, 1F);
         }, 4 * 20L);
+        
+         */
 
         String joinMessage = CC.format(PlaceholderAPI.setPlaceholders(player, config.getString("join_leave.join_message", "")));
         if (!joinMessage.isEmpty()) {
